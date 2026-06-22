f8984f3715a2b32578ad51e7d088b10c0fd1b69f
Maria <minervalafuente@hotmail.com>
2026-06-22 20:30:49 +0200

    Finished the first version of the UITest. First MVP.

--- src/main/java/TextUI.java
+++ src/main/java/TextUI.java
@@ -15,7 +15,7 @@ public class TextUI {
 
     /**
      * Imports the Scanner and the SimpleDictionary objects to interact with the user thu the console
-     * @param scanner
+     * @param scanner Allows to read imputs from the keyboard
      * @param dictionary SimpleDictionary object. Uses a hashmap to pair words with their translations.
      */
     public TextUI (Scanner scanner, SimpleDictionary dictionary){
@@ -27,23 +27,63 @@ public class TextUI {
 
     /**
      * Reads commands from the user and displays different messages accordingly
+     * "add" saves two instances of pairs key-value in the dictionary
+     * "search" looks for the value pair of the key word given and print it. If it's null, will show a diff msg
      */
     public void start(){
 
         String word;
+        String translation;
 
         System.out.println("DICTIONARY APP");
         System.out.println("--------------");
-        System.out.println("");
+        System.out.println();
 
-        System.out.println("Command: ");
-        word= scanner.nextLine();
+        while (true) {
 
-        if(word.equals("end")){
-            System.out.println("Bye, bye!");
+            System.out.println("Command: ");
+            word = scanner.nextLine();
 
-        }else{
-            System.out.println("Command Unknown");
+            // finishes the program
+            if (word.equalsIgnoreCase("end")) {
+                System.out.println("Bye, bye!");
+                break;
+
+            // the words get added to the dictionary object twice, for the search in each language
+            } else if (word.equalsIgnoreCase("add")) {
+                System.out.println("Word: ");
+                word = scanner.nextLine();
+
+                System.out.println("Translation: ");
+                translation = scanner.nextLine();
+
+                dictionary.add(word, translation);
+                dictionary.add(translation, word);
+
+            } else if (word.equalsIgnoreCase("search")){
+
+                if (dictionary.isEmpty()){
+                    System.out.println("The dictionary is empty");
+
+                } else {
+
+                    System.out.println("Word to be translated: ");
+                    word = scanner.nextLine();
+                    translation = dictionary.translate(word);
+
+                    if (translation == null) {
+                        System.out.println("The word " + word + " is not yet in the dictionary");
+
+                    } else {
+                        System.out.println("Translation: " + translation);
+                    }
+                }
+
+            } else {
+                System.out.println("Command Unknown");
+
+            }
+
         }
 
     }
